02c5889853aaa6e8d6f93ec0ee7d943033d9cf45
sam <selman@gg.com>
2024-09-02 20:03:42 -0600

    feat: auto generating docs with swagger

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -48,29 +48,10 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedF
 		ORDER BY p.created_at ` + fq.Sort + `
 		LIMIT $2 OFFSET $3
 	`
-
-	// 	query := `
-	// 	SELECT
-	// 		p.id, p.user_id, p.title, p.content, p.created_at, p.version, p.tags,
-	// 		u.username,
-	// 		COUNT(c.id) AS comments_count
-	// 	FROM posts p
-	// 	LEFT JOIN comments c ON c.post_id = p.id
-	// 	LEFT JOIN users u ON p.user_id = u.id
-	// 	JOIN followers f ON f.follower_id = p.user_id OR p.user_id = $1
-	// 	WHERE
-	// 		f.user_id = $1 AND
-	// 		(p.title ILIKE '%' || $4 || '%' OR p.content ILIKE '%' || $4 || '%') AND
-	// 		(p.tags @> $5 OR $5 = '{}')
-	// 	GROUP BY p.id, u.username
-	// 	ORDER BY p.created_at ` + fq.Sort + `
-	// 	LIMIT $2 OFFSET $3
-	// `
 	fmt.Println(query)
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
 	defer cancel()
 
-	// rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset, fq.Search, pq.Array(fq.Tags))
 	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset, fq.Search)
 	if err != nil {
 		return nil, err
@@ -80,9 +61,7 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedF
 
 	var feed []PostWithMetadata
 	for rows.Next() {
-
 		var p PostWithMetadata
-
 		err := rows.Scan(
 			&p.ID,
 			&p.UserID,
